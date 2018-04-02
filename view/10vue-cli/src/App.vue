0a4dcb24a77e4338e4e13ea82b120a6c26f35354
Administrator <zhangtanlin90@126.com>
2018-04-03 00:04:02 +0800

    一些Vue基础配置和vue-cli脚手架配置 Signed-off-by: Administrator <zhangtanlin90@126.com>

---
+++ view/10vue-cli/src/App.vue
@@ -0,0 +1,39 @@
+<!--
+	作者：zhangtanlin90@126.com
+	时间：2018-04-02
+	描述：每一个模块里面都包含模板【template】、行为【script】、样式【style】。
+				template节点不会被渲染到DOM里，
+				template里面有且只有一个根标签，根节点有多少子节点不限制。
+-->
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+	/**
+	 * 导入组件HelloWorld，
+	 * 注意导入组件后，一定要components【注册】一下。
+	 */
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
